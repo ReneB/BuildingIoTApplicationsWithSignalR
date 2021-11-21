dabc8e341e4a6128bab48633289731be747fd91a
René van den Berg <rene@ranj.nl>
2021-11-21 11:15:15 +0100

    Add basic implementation of a SignalR Client that keeps the connection alive and sends Ping packets every 30 seconds

--- client/Program.cs
+++ client/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+int pingDelayInSeconds = 30;
+
+var identifier = Guid.NewGuid().ToString();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:5001/main")
+    .WithAutomaticReconnect()
+    .Build();
+
+await connection.StartAsync();
+
+Console.WriteLine("Connection established");
+
+while (true) {
+    await Task.Delay(pingDelayInSeconds * 1000);
+
+    if (connection.State == HubConnectionState.Connected) {
+        Console.WriteLine("Ping!");
+
+        await connection.SendAsync("Ping", identifier);
+    } else {
+        Console.WriteLine("Connection down; skipping ping.");
+    }
+}
