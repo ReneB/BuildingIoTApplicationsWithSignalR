f80619cef804ba57a50e9d27620dddfaea2f5079
René van den Berg <rene@ranj.nl>
2021-11-21 11:52:40 +0100

    Add basic 'client overview' page to server

---
+++ server/Pages/ClientOverview.cshtml.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Pages;
+
+public class ClientOverviewModel : PageModel {
+    public void OnGet() { }
+}
---
+++ server/wwwroot/js/client-overview.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var clients = [];
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/main").build();
+
+function renderClientList() {
+    var clientList = document.getElementById("clientList");
+    clientList.innerHTML = '';
+
+    clients.forEach(client => {
+        var li = document.createElement("li");
+
+        document.getElementById("clientList").appendChild(li);
+
+        li.textContent = `${client.id} was seen at ${client.lastSeen}`;
+    });
+}
+
+connection.on("RegisterDevicePresence", function (deviceId, timestamp) {
+    var knownClient = clients.find((client) => client.id === deviceId);
+    if (knownClient) {
+        knownClient.lastSeen = new Date(timestamp);
+    } else {
+        var newClient = {
+            id: deviceId,
+            lastSeen: new Date(timestamp)
+        };
+
+        clients.push(newClient);
+    }
+
+    renderClientList();
+});
+
+connection.start().then(function () {
+    connection.invoke("RegisterAsMaster");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
