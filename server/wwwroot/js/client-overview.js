891569a3495103fdca65c98c9b57cad2354bb30b
René van den Berg <rene@ranj.nl>
2021-11-21 23:43:21 +0100

    Mild improvement: don't use string literals to function as 'meaningful' values

--- server/wwwroot/js/client-overview.js
+++ server/wwwroot/js/client-overview.js
@@ -4,6 +4,11 @@ var clients = [];
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/main").build();
 
+const ClientStates = {
+    online: "online",
+    offline: "offline"
+};
+
 function renderClientList() {
     var clientList = document.getElementById("clientList");
     clientList.innerHTML = '';
@@ -14,6 +19,8 @@ function renderClientList() {
         document.getElementById("clientList").appendChild(li);
 
         li.textContent = `${client.id} was seen at ${client.lastSeen}`;
+
+        li.setAttribute("class", client.status);
     });
 }
 
@@ -21,6 +28,7 @@ connection.on("RegisterDevicePresence", (deviceId, timestamp) => {
     var knownClient = clients.find((client) => client.id === deviceId);
     if (knownClient) {
         knownClient.lastSeen = new Date(timestamp);
+        knownClient.status = ClientStates.online;
     } else {
         var newClient = {
             id: deviceId,
@@ -34,7 +42,12 @@ connection.on("RegisterDevicePresence", (deviceId, timestamp) => {
 });
 
 connection.on("RegisterDeviceOffline", (deviceId) => {
-    clients = clients.filter((client) => client.id !== deviceId);
+    var knownClient = clients.find((client) => client.id === deviceId);
+    if (knownClient === null || knownClient === undefined) {
+        return;
+    }
+
+    knownClient.status = ClientStates.offline;
 
     renderClientList();
 });
