9de7c803438857fe2fc9b92f89a463d1b2a13180
René van den Berg <rene@ranj.nl>
2021-12-03 01:50:56 +0100

    Rework Client from a linear, overly procedural script to have at least a Client class and a bunch of methods, to add some structure to the darn thing

--- client/Program.cs
+++ client/Program.cs
@@ -1,56 +1,72 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 
-static async Task Ping(HubConnection connection, string deviceId) {
-    await connection.SendAsync(nameof(Ping), deviceId);
-}
+await new Client().Start();
 
-int pingDelayInSeconds = 30;
-int initialRetryDelayInSeconds = 5;
+public class Client {
+    private const int pingDelayInSeconds = 30;
+    private const int initialRetryDelayInSeconds = 5;
 
-var identifier = Guid.NewGuid().ToString();
+    private HubConnection connection;
+    private string identifier = Guid.NewGuid().ToString();
+    private string url = "https://localhost:5001/main";
 
-var connection = new HubConnectionBuilder()
-    .WithUrl("https://localhost:5001/main")
-    .WithAutomaticReconnect()
-    .Build();
-
-while (connection.State != HubConnectionState.Connected) {
-    try {
-        await connection.StartAsync();
-    } catch (HttpRequestException) {
-        Console.WriteLine($"Failed to establish connection, will retry in {initialRetryDelayInSeconds} seconds");
-        await Task.Delay(initialRetryDelayInSeconds * 1000);
-    }
-}
-
-Console.WriteLine("Connection established");
-
-connection.On<string, string, byte[]>(nameof(Client.Messages.Announce), async (announcementId, announcementText, byteArrayMp3) => {
-    Console.WriteLine($"Receiving announcement: {announcementText}");
-
-    var fileName = $"Temp/{announcementId}.mp3";
-    await File.WriteAllBytesAsync(fileName, byteArrayMp3);
-
-    Console.WriteLine($"Reassembled incoming announcement as {fileName}");
-
-    var player = new NetCoreAudio.Player();
-    await player.Play(fileName).WaitAsync(TimeSpan.FromSeconds(30));
-});
-
-while (true) {
-    if (connection.State == HubConnectionState.Connected) {
-        Console.WriteLine("Ping!");
-
-        await Ping(connection, identifier);
-    } else {
-        Console.WriteLine("Connection down; skipping ping.");
+    public Client() {
+        connection = new HubConnectionBuilder()
+            .WithUrl(url)
+            .WithAutomaticReconnect()
+            .Build();
     }
 
-    await Task.Delay(pingDelayInSeconds * 1000);
-}
+    public async Task Start() {
+        await Connect();
 
-namespace Client {
-    public enum Messages {
-        Announce
+        await KeepAlive();
+    }
+
+    private async Task Connect() {
+        while (connection.State != HubConnectionState.Connected) {
+            try {
+                await connection.StartAsync();
+            } catch (HttpRequestException) {
+                Console.WriteLine($"Failed to establish connection, will retry in {initialRetryDelayInSeconds} seconds");
+                await Task.Delay(initialRetryDelayInSeconds * 1000);
+            }
+        }
+
+        Console.WriteLine("Connection established");
+    }
+
+    private async Task KeepAlive() {
+        while (true) {
+            if (connection.State == HubConnectionState.Connected) {
+                Console.WriteLine("Ping!");
+
+                await Ping(connection, identifier);
+            } else {
+                Console.WriteLine("Connection down; skipping ping.");
+            }
+
+            await Task.Delay(pingDelayInSeconds * 1000);
+        }
+    }
+
+    public void AddEventListeners(HubConnection connection) {
+        connection.On<string, string, byte[]>(nameof(Announce), Announce);
+    }
+
+    private async Task Announce(string announcementId, string announcementText, byte[] byteArrayMp3) {
+        Console.WriteLine($"Receiving announcement: {announcementText}");
+
+        var fileName = $"Temp/{announcementId}.mp3";
+        await File.WriteAllBytesAsync(fileName, byteArrayMp3);
+
+        Console.WriteLine($"Reassembled incoming announcement as {fileName}");
+
+        var player = new NetCoreAudio.Player();
+        await player.Play(fileName).WaitAsync(TimeSpan.FromSeconds(30));
+    }
+
+    private async Task Ping(HubConnection connection, string deviceId) {
+        await connection.SendAsync(nameof(Ping), deviceId);
     }
 }
