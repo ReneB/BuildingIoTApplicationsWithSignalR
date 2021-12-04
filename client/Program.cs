67eed7cc7f676273dba4beab53ef7af301f3cc7f
René van den Berg <rene@ranj.nl>
2021-12-04 22:30:40 +0100

    Move client main file into a namespace

--- client/Program.cs
+++ client/Program.cs
@@ -1,89 +1,91 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 
-await new Client().Start();
+await new Client.Client().Start();
 
-public class Client {
-    private const TimeSpan pingDelay = TimeSpan.FromSeconds(30);
-    private const TimeSpan initialRetryDelay = TimeSpan.FromSeconds(5);
+namespace Client {
+    public class Client {
+        private const TimeSpan pingDelay = TimeSpan.FromSeconds(30);
+        private const TimeSpan initialRetryDelay = TimeSpan.FromSeconds(5);
 
-    private HubConnection connection;
-    private readonly string identifier = Guid.NewGuid().ToString();
-    private readonly int gateNumber = RandomGateNumber();
-    private string url = "https://localhost:5001/main";
+        private HubConnection connection;
+        private readonly string identifier = Guid.NewGuid().ToString();
+        private readonly int gateNumber = RandomGateNumber();
+        private string url = "https://localhost:5001/main";
 
-    public Client() {
-        connection = new HubConnectionBuilder()
-            .WithUrl(url)
-            .WithAutomaticReconnect()
-            .Build();
-    }
+        public Client() {
+            connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .WithAutomaticReconnect()
+                .Build();
+        }
 
-    public async Task Start() {
-        Console.WriteLine($"Current client id: {identifier}");
-        Console.WriteLine($"Gate number: {gateNumber}");
+        public async Task Start() {
+            Console.WriteLine($"Current client id: {identifier}");
+            Console.WriteLine($"Gate number: {gateNumber}");
 
-        AddEventListeners();
+            AddEventListeners();
 
-        await Connect();
+            await Connect();
 
-        await KeepAlive();
-    }
+            await KeepAlive();
+        }
 
-    private async Task Connect() {
-        while (connection.State != HubConnectionState.Connected) {
-            try {
-                await connection.StartAsync();
+        private async Task Connect() {
+            while (connection.State != HubConnectionState.Connected) {
+                try {
+                    await connection.StartAsync();
 
-                await RegisterDevice();
-            } catch (HttpRequestException) {
-                Console.WriteLine($"Failed to establish connection, will retry in {initialRetryDelay.Seconds} seconds");
-                await Task.Delay(initialRetryDelay);
+                    await RegisterDevice();
+                } catch (HttpRequestException) {
+                    Console.WriteLine($"Failed to establish connection, will retry in {initialRetryDelay.Seconds} seconds");
+                    await Task.Delay(initialRetryDelay);
+                }
+            }
+
+            Console.WriteLine("Connection established");
+        }
+
+        private async Task KeepAlive() {
+            while (true) {
+                if (connection.State == HubConnectionState.Connected) {
+                    Console.WriteLine("Ping!");
+
+                    await Ping();
+                } else {
+                    Console.WriteLine("Connection down; skipping ping.");
+                }
+
+                await Task.Delay(pingDelay);
             }
         }
 
-        Console.WriteLine("Connection established");
-    }
-
-    private async Task KeepAlive() {
-        while (true) {
-            if (connection.State == HubConnectionState.Connected) {
-                Console.WriteLine("Ping!");
-
-                await Ping();
-            } else {
-                Console.WriteLine("Connection down; skipping ping.");
-            }
-
-            await Task.Delay(pingDelay);
+        public void AddEventListeners() {
+            connection.On<string, string, byte[]>(nameof(Announce), Announce);
+            connection.Reconnected += async (_) => await RegisterDevice();
         }
-    }
 
-    public void AddEventListeners() {
-        connection.On<string, string, byte[]>(nameof(Announce), Announce);
-        connection.Reconnected += async (_) => await RegisterDevice();
-    }
+        private async Task Announce(string announcementId, string announcementText, byte[] byteArrayMp3) {
+            Console.WriteLine($"Receiving announcement: {announcementText}");
 
-    private async Task Announce(string announcementId, string announcementText, byte[] byteArrayMp3) {
-        Console.WriteLine($"Receiving announcement: {announcementText}");
+            var fileName = $"Temp/{announcementId}.mp3";
+            await File.WriteAllBytesAsync(fileName, byteArrayMp3);
 
-        var fileName = $"Temp/{announcementId}.mp3";
-        await File.WriteAllBytesAsync(fileName, byteArrayMp3);
+            Console.WriteLine($"Reassembled incoming announcement as {fileName}");
 
-        Console.WriteLine($"Reassembled incoming announcement as {fileName}");
+            var player = new NetCoreAudio.Player();
+            await player.Play(fileName).WaitAsync(TimeSpan.FromSeconds(30));
+        }
 
-        var player = new NetCoreAudio.Player();
-        await player.Play(fileName).WaitAsync(TimeSpan.FromSeconds(30));
-    }
+        private async Task Ping() {
+            await connection.SendAsync(nameof(Ping));
+        }
 
-    private async Task Ping() {
-        await connection.SendAsync(nameof(Ping));
-    }
+        private async Task RegisterDevice() {
+            await connection.SendAsync(nameof(RegisterDevice), identifier, gateNumber);
+        }
 
-    private async Task RegisterDevice() {
-        await connection.SendAsync(nameof(RegisterDevice), identifier, gateNumber);
-    }
-
-    private static int RandomGateNumber() {
-        return 1 + new Random().Next(10);
+        private static int RandomGateNumber() {
+            return 1 + new Random().Next(10);
+        }
     }
 }
